c57a7ccae02ae177b6b8a0d3e4803dd62aa60a78
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-28 16:00:04 +0100

    ADD methods nextHero and prevHero

--- js/script.js
+++ js/script.js
@@ -10,6 +10,19 @@ createApp({
         }
     },
     methods:{
-
+        nextHero(){
+            if(this.currentIndex < this.slides.length - 1){
+                this.currentIndex++;
+            }else{
+                this.currentIndex = 0;
+            }
+        },
+        prevNext(){
+            if(this.currentIndex > 0){
+                this.currentIndex--;
+            }else{
+                this.currentIndex.length - 1;
+            }
+        }
     }
 }).mount('#app');
